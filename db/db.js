3397affd46fa268e3a15815b449925848214afd0
Sam Sharff <100388192+SamSharff@users.noreply.github.com>
2022-04-28 09:19:00 -0400

    tests merge conflict between vs code and web editor

--- db/db.js
+++ db/db.js
@@ -2,7 +2,11 @@ const todos = [
   {
     id: 1,
     title: "lunch",
+<<<<<<< HEAD
+    description: "Go for lunc by 2pmtest",
+=======
     description: "Go for lunch by 2pm",
+>>>>>>> 6d633b59fa867543014ea768fa19dd0f8d3997c7
   },
 ];
 
