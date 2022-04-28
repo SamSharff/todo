296fb42be76355afc1a87b3b4c94dfaf71c9c9b1
Sam Sharff <100388192+SamSharff@users.noreply.github.com>
2022-04-28 09:23:36 -0400

    tests change in web editor to see if show in local vs code

--- db/db.js
+++ db/db.js
@@ -2,11 +2,7 @@ const todos = [
   {
     id: 1,
     title: "lunch",
-<<<<<<< HEAD
-    description: "Go for lunc by 2pmtest",
-=======
-    description: "Go for lunch by 2pm",
->>>>>>> 6d633b59fa867543014ea768fa19dd0f8d3997c7
+    description: "Go for lunch by 2pm testtest",
   },
 ];
 
